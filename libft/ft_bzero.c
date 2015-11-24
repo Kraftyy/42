e244428f1a8573c03be4af8caf578b640ae3bd45
Nicolas DELMATTO <ndelmatt@e1r1p12.42.fr>
2015-11-24 11:23:54 +0100

    Update of bzero function

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,22 +6,13 @@
 /*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 15:55:15 by ndelmatt          #+#    #+#             */
-/*   Updated: 2015/11/23 16:08:40 by ndelmatt         ###   ########.fr       */
+/*   Updated: 2015/11/24 11:22:57 by ndelmatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void		ft_bzero(void *s, size_t n)
 {
-	size_t		count;
-
-	count = 0;
-	if (n == 0)
-		return;
-	while (count < n)
-	{
-		s[count] = 0;
-		count++;
-	}
+	s = ft_memset(s, 0, n);
 }
