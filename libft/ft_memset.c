1aadfb4c0403464b3e446f0bf3b3d5d72174083e
Nicolas DELMATTO <ndelmatt@e1r1p12.42.fr>
2015-11-24 11:09:28 +0100

    Udpate of Memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,21 +6,23 @@
 /*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 15:22:31 by ndelmatt          #+#    #+#             */
-/*   Updated: 2015/11/23 15:54:57 by ndelmatt         ###   ########.fr       */
+/*   Updated: 2015/11/24 11:08:06 by ndelmatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void		*ft_memset(void *b, int c, size_t len)
 {
-	int		count;
-
-	count = 0;
-	while (count < len)
+	size_t			i;
+	unsigned char	*tmp;
+	
+	i = 0;
+	tmp = (unsigned char*)b;
+	while (i < len)
 	{	
-		((unsigned char )b[count]) = (unsigned char)c;
-		count++;
+		tmp[i] = (unsigned char)c;
+		i++;
 	}
-	return b;
+	return (b);
 }
