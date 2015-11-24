bada2d1e12fddbac3d7a70f86ce89e975c432f47
Nicolas DELMATTO <ndelmatt@e1r1p12.42.fr>
2015-11-24 11:54:31 +0100

    Update of memccpy functiom

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,27 +6,31 @@
 /*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 16:15:18 by ndelmatt          #+#    #+#             */
-/*   Updated: 2015/11/23 16:27:38 by ndelmatt         ###   ########.fr       */
+/*   Updated: 2015/11/24 11:52:54 by ndelmatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-void		*ft_memccpy(void *dst, const void *src, int c,
-				size_t n)
+void		*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t		count;
+	size_t			i;
+	unsigned char 	*td;
+	unsigned char	*ts;
 
-	count = 0;
-	while (count < n)
+	i = 0;
+	td = (unsigned char*)dst;
+	ts = (unsigned char*)src;
+	while (i < n)
 	{
-		if ((unsigned char)src[count] == (unsigned char)c)
+		if (ts[i] == (unsigned char)c)
 		{
-			count++;
-			return (dst[count]);
+			dst++;
+			return (dst);
 		}
-		dst[count] = src[count];
-		count++;
+		td[i] = ts[i];
+		i++;
+		dst++;
 	}
 	return (NULL);
 }
