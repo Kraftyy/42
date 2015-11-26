a8b7f65c127672a024245d3f3955ac71c2696694
Nicolas DELMATTO <ndelmatt@e1r1p9.42.fr>
2015-11-26 14:55:16 +0100

    v1 | ft_strmapi.c | first version

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/26 14:50:11 by ndelmatt          #+#    #+#             */
+/*   Updated: 2015/11/26 14:54:53 by ndelmatt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*mem;
+
+	if (!(mem = (char *)malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		mem[i] = f(i, (char)s);
+		s++;
+		i++;
+	}
+	return (mem);
+}
