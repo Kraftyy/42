c5215233126d4c41b73fcc68a42d8b15598e7945
Nicolas DELMATTO <ndelmatt@e1r1p9.42.fr>
2015-11-26 14:49:36 +0100

    v1

---
+++ libft/ft_strmap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/26 14:43:02 by ndelmatt          #+#    #+#             */
+/*   Updated: 2015/11/26 14:49:03 by ndelmatt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strmap(char const *s, char (*f)(char *))
+{
+	int		i;
+	char	*mem;
+
+	if (!(mem = (char *)malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		mem[i] = f((char *)s);
+		s++;
+		i++;
+	}
+	return (mem);
+}
