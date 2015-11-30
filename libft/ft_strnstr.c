a00a6c3f8e9110701157ec7346b9be2bdc9a99f6
Nicolas DELMATTO <ndelmatt@e1r1p12.42.fr>
2015-11-30 16:01:49 +0100

    v1.1 | ft_strnstr.c | Update of counters

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 15:06:45 by ndelmatt          #+#    #+#             */
-/*   Updated: 2015/11/25 15:30:01 by ndelmatt         ###   ########.fr       */
+/*   Updated: 2015/11/30 16:00:27 by ndelmatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,16 @@ char		*ft_strnstr(const char *s1, const char *s2, size_t n)
 	if (!*s2)
 		return ((char *)s1);
 	i = 0;
-	while (s1[i] && n > 0)
+	while (s1[i] && i < n)
 	{
 		y = 0;
-		while (s1[i] == s2[y] && (s2 != '\0' || s1 != '\0'))
+		while (s1[i + y] == s2[y] && (s2[y] || s1[i + y]) && ((n - i) - y) > 0)
 		{
 			y++;
-			i++;
-			n--;
 		}
 		if (s2[y] == '\0')
-			return ((char *)s1 + (i - y));
+			return ((char *)s1 + i);
 		i++;
-		n--;
 	}
 	return (NULL);
 }
