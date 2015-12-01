4f0bb8595c028859b0767db4531b21e673b466d6
Kraftyy <nicolas.delmatto@gmail.com>
2015-12-01 13:19:12 +0100

    updates

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -14,5 +14,17 @@
 
 size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
-	
+	size_t	sizemax;
+	size_t	res;
+	size_t	i;
+
+	sizemax = size - ft_strlen(dst) - 1;
+	res = ft_strlen(src);
+	i = 0;
+	while (dst[i] && i < size)
+		i++;
+	res += ((size > i) ? i : size );
+	if ((int)sizemax > 0)
+		ft_strncat(dst, src, sizemax);
+	return (res);
 }
