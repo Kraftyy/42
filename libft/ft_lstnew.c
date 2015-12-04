48c3e77a8e9daf1078c689c95bf690aa677eba7f
Nicolas DELMATTO <ndelmatt@e1r1p8.42.fr>
2015-12-04 22:27:36 +0100

    updates

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/04 22:03:38 by ndelmatt          #+#    #+#             */
+/*   Updated: 2015/12/04 22:26:14 by ndelmatt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list		*ft_lstnew(void const *content, size_t content_size)
@@ -6,15 +18,15 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	
 	if (!(listnew = (t_list*)malloc(sizeof(t_list))))
 		return (NULL);
-	newlist->next = NULL;
+	listnew->next = NULL;
 	if (content == NULL)
 	{
-		newlist->content = NULL;
-		newlist->content_size = 0;
+		listnew->content = NULL;
+		listnew->content_size = 0;
 	}
 	else
 	{
-		if (!(listnew = (t_list*)malloc(sizeof(content_size))))
+		if (!(listnew->content = (t_list*)malloc(sizeof(content_size))))
 			return (NULL);
 		ft_memmove(listnew->content, content, content_size);
 		listnew->content_size = content_size;
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 14:18:37 by ndelmatt          #+#    #+#             */
-/*   Updated: 2015/11/30 22:31:14 by ndelmatt         ###   ########.fr       */
+/*   Updated: 2015/12/04 22:21:32 by ndelmatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void		*ft_memrcpy(void *dst, const void *src, size_t n);
 void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memalloc(size_t size);
 void		ft_memdel(void **ap);
 
@@ -41,8 +41,8 @@ char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strstr(const char *s1, const char *s2);
 char		*ft_strnstr(const char *s1, const char *s2, size_t n);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strnew(size_t size);
 void		ft_strdel(char **as);
 void		ft_strclr(char *s);
@@ -50,8 +50,8 @@ void		ft_striter(char *s, void (*f)(char *));
 void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 char		*ft_strmap(char const *s, char (*f)(char));
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-int		ft_strequ(char const *s1, char const *s2);
-int		ft_strnequ(char const *s1, char const *s2, size_t n);
+int			ft_strequ(char const *s1, char const *s2);
+int			ft_strnequ(char const *s1, char const *s2, size_t n);
 char		*ft_strsub(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s);
@@ -68,7 +68,7 @@ void		ft_putendl_fd(char const *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
 
 /*Convertors*/
-int		ft_atoi(const char *str);
+int			ft_atoi(const char *str);
 char		*ft_itoa(int n);
 
 /*Char tests*/
@@ -88,5 +88,9 @@ typedef	struct		s_list
 	size_t		content_size;
 	struct s_list	*next;
 }			t_list;
+
+/*List functions*/
+t_list		*ft_lstnew(void const *content, size_t content_size);
+void		ft_lstadd(t_list **alst, t_list *new);
 
 #endif
