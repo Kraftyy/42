ba25be8338cb64bfe80cb844135b76b2552d0006
Nicolas DELMATTO <ndelmatt@e1r1p10.42.fr>
2015-12-17 15:33:52 +0100

    updates

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 14:18:37 by ndelmatt          #+#    #+#             */
-/*   Updated: 2015/12/17 11:28:33 by ndelmatt         ###   ########.fr       */
+/*   Updated: 2015/12/17 15:28:59 by ndelmatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,6 +94,8 @@ int					ft_isdigit(int c);
 int					ft_isalnum(int c);
 int					ft_isascii(int c);
 int					ft_isprint(int c);
+int					ft_islower(int c);
+int					ft_isupper(int c);
 
 /*
 ** Char transformations
@@ -102,9 +104,17 @@ int					ft_toupper(int c);
 int					ft_tolower(int c);
 
 /*
+** Various
+*/
+
+void				ft_swap(int a, int b);
+
+/*
 ** List functions
 */
 t_list				*ft_lstnew(void const *content, size_t content_size);
+void				ft_lstdel(t_list **alst, void (*del) (void *, size_t));
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f) (t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f) (t_list *elem));
