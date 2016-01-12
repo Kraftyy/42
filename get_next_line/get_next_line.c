1a43627f7b48c31f56e4a0e15da3863fdd75358d
Nicolas DELMATTO <ndelmatt@e1r5p19.42.fr>
2016-01-12 14:12:59 +0100

    youuou

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,18 +6,26 @@
 /*   By: ndelmatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/11 17:40:58 by ndelmatt          #+#    #+#             */
-/*   Updated: 2016/01/11 18:52:56 by ndelmatt         ###   ########.fr       */
+/*   Updated: 2016/01/12 14:12:47 by ndelmatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void				add_line(t_list *buff)
+{
+
+}
 
 int					get_next_line(int const fd, char **line)
 {
 	int				ret;
 	static t_list	*buff;	
 
+	if (fd < 0 || !line || BUF_SIZE < 1)
+		return (-1);
 	ret = read(fd, buff, BUFF_SIZE);
-
+	if (ret != 0)
+		add_line(buff);
 	return (ret);
 }
